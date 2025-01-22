126d9fdad14482cfb809e1e98c5d84d7608e40f4
chandanashree9 <chandanashree9@yahoo.com>
2025-01-22 15:16:08 -0500

    added yml file

--- src/main/java/com/tekarch/TafUserService/resttemplate/UserRestTemplate.java
+++ src/main/java/com/tekarch/TafUserService/resttemplate/UserRestTemplate.java
@@ -3,6 +3,7 @@ package com.tekarch.TafUserService.resttemplate;
 import com.tekarch.TafUserService.model.UserRequest;
 import com.tekarch.TafUserService.model.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -13,26 +14,27 @@ public class UserRestTemplate {
     @Autowired
     private RestTemplate restTemplate;
 
-    private final String BASE_URL="http://localhost:8083/api";
+    @Value("${api.base.url}")
+    private final String BASE_URL;
+
+    public UserRestTemplate(@Value("${api.base.url}") String baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
     public UserResponse createFlight(UserRequest requestUser){
-        String url=BASE_URL+"/users/register";
-        UserResponse response = restTemplate.postForObject(url,requestUser, UserResponse.class);
-        System.out.println("User response:"+response);
+        UserResponse response =restTemplate.postForObject(BASE_URL+"/register",requestUser,UserResponse.class);
+        System.out.println("create flight:"+response);
         return response;
     }
 
     public UserResponse getUserById(Long userId){
-        String url=BASE_URL+"/users/{userId}";
-        System.out.println("Get User url:"+url);
-        return restTemplate.getForObject(url,UserResponse.class,userId);
+        return restTemplate.getForObject(BASE_URL+"/{userId}",UserResponse.class,userId);
     }
 
     public ResponseEntity<UserRequest> updateUser(Long userId, UserRequest updatedUser){
-        String url=BASE_URL+"/users/{userId}";
         HttpHeaders headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<UserRequest> requestEntity=new HttpEntity<>(updatedUser,headers);
-        return restTemplate.exchange(url, HttpMethod.PUT,requestEntity,UserRequest.class,userId);
+        return restTemplate.exchange(BASE_URL+"/{userId}", HttpMethod.PUT,requestEntity,UserRequest.class,userId);
     }
 }
