bf06d78ba4edbb3bd6611540011da2afde4cef35
chandanashree9 <chandanashree9@yahoo.com>
2025-01-22 10:55:03 -0500

    added yml file

--- src/main/java/com/tekarch/TafUserService/model/UserResponse.java
+++ src/main/java/com/tekarch/TafUserService/model/UserResponse.java
@@ -1,8 +1,10 @@
 package com.tekarch.TafUserService.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
 
     private Long id;
